86b6cb4fd030efc042f93b480d95b28303d8f97e
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2025-03-27 20:15:16 +0100

    Database and background img added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { useState } from 'react';
+import quotes from "./database";
 import "./App.css";
 
+
 function App() {
-  return <div className="App"></div>;
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  };
+
+  const [quote, setQuote] = useState(getRandomQuote());
+
+  const handleNewQuote = () => {
+    setQuote(getRandomQuote());
+  };
+
+  return (
+    <div>
+    <div>
+      <p >"{quote.quote}"</p>
+      <p >– {quote.by}</p>
+      <button  onClick={handleNewQuote}>New Quote</button>
+    </div>
+  </div>
+  )
 }
 
 export default App;
---
+++ src/database.js
@@ -0,0 +1,34 @@
+ const quotes = [
+    {
+        id: 1, 
+        quote: "You only live once, but if you do it right, once is enought.",
+        by: "Mae West"
+    },
+    {
+        id: 2, 
+        quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+        by: "Helen Keller"
+    },
+    {
+        id: 3, 
+        quote: "To live is the rarest thing in the world. Most people exist, that is all.",
+        by: "Oscar Wilde"
+    },
+    {
+        id: 4, 
+        quote: "Everything you´ve ever wanted is on the other side of fear.",
+        by: "George Addair"
+    },  
+    {
+        id: 5, 
+        quote: "The only impossible journey is the one you never begin.",
+        by: "Tony Robbins"
+    },
+    {
+        id: 6, 
+        quote: "Keep your face always toward the sunshine—and shadows will fall behind you.",
+        by: "Walt Whitman"
+    },
+ ];
+
+ export default quotes;
