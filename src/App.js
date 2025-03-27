19c0002f69a4c2bf297afb6f9565a65c2897bd98
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2025-03-27 20:30:43 +0100

    Css changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import quotes from "./database";
-import "./App.css";
-
 
 function App() {
-
   const getRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     return quotes[randomIndex];
@@ -17,14 +14,28 @@ function App() {
   };
 
   return (
-    <div>
-    <div>
-      <p >"{quote.quote}"</p>
-      <p >– {quote.by}</p>
-      <button  onClick={handleNewQuote}>New Quote</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#f3eddf",
+          width: "500px",
+          height: "300px",
+          padding: "20px",
+        }}
+      >
+        <p style={{fontFamily: "Times New Roman", fontSize: "30px"}}>"{quote.quote}"</p>
+        <p>– {quote.by}</p>
+        <button onClick={handleNewQuote}>New Quote</button>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default App;
